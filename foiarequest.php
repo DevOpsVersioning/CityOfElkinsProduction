440a11a7ad0d94e2919242a69fd1d41d6c045792
casto-samuel <castos@dewv.edu>
2015-09-18 11:18:13 -0400

    Create foiarequest.php
    initial commit of foiarequest.php

---
+++ foiarequest.php
@@ -0,0 +1,49 @@
+<?php require("json_form_scripts.php");?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+	    	<meta charset="UTF-8">
+	 	<title>Freedom of Information Act Request</title>
+        	<link rel="icon" href="http://cityofelkinswv.com/templates/default/favicon.png">
+		<link rel="stylesheet" href="elkins.css" type="text/css">
+    	</head>
+<body>
+	<header></header>
+	<article>
+		<h1>Freedom of Information Act Request</h1>
+		<h2>City of Elkins FOIA Fees</h2>
+		<h3>Approved by council December 6, 2012</h3>
+		<p>1. In the event of a request for the inspection and copying of city records under W. Va. Code §29B-1-1et seq., as amended (commonly known as the Freedom of Information Act), fees will be charged to the requester for searching for and reproducing records, in accordance with the following:</p>
+		<p>1.a. Copying or printing costs shall be twenty-five cents ($0.25) per page; provided that the first 5 pages are free (limit: one free request per quarter);</p>
+		<p>1.b. No charge will be made if the total fee to process a request is one dollar and twenty-five cents ($1.25) or less;</p>
+		<p>1.c. The charge for copying materials that must be reproduced outside of city hall facilities shall be the actual cost to reproduce the record;</p>
+		<p>1.d. Mailing or delivery charges may be charged, at the city’s actual cost, in connection with sending the requested information to a requester;</p>
+		<p>1.f. Diskette, tape, flash drive or other storage media will be provided at the city’s actual cost;</p>
+		<p>1.g. A fee of $25 per hour, or quarter fraction thereof, for time spent in locating and duplicating requested records. The first half hour used to locate such records is free. Professional services (such as legal, engineering, accounting, etc.) related to a request will be charged at the city’s actual cost.</p>
+		<p>1.h. An invoice for the total amount of fees will be sent to the requester. Payment shall be made by check or money order payable to “City of Elkins”;</p>
+		<p>1.i. Total or partial pre-payments may be required if the cost to reproduce the information will exceed twenty-five dollars ($25);</p>
+		<p>1.j. Total or partial pre-payments may be required if there are unpaid fees on one or more request by the same requester that exceed twenty-five dollars ($25); and</p>
+		<p>1.k.Any requester who claims that he or she cannot afford to pay the fees established by this rule may file an affidavit with the city clerk stating the reason the requester cannot afford to pay. If the city clerk agrees, the request will be processed in accordance with this rule and as if the fees have been paid.</p>
+		<form>
+			<label>Name of Requester <input type="text" name="requester" class="submitter" required></label><br>
+			<label>Mailing Address <input type="text" name="address" required></label><br> 
+			<label>City <input type="text" name="city" required></label><br>	
+			<label>State <input type="text" name="state" required></label><br>
+			<label>Zip <input type="text" name="zip" required></label><br>
+			<label>Telephone <input type="tel" name="telephone" required></label><br>
+			<label>E-mail <input type="email" name="email" required></label><br>
+			<label>Fax (Optional) <input type="tel" name="fax"></label><br>
+			<label>Records Requested (Provide as much specific detail as possible) <textarea rows="5" cols="100" name="details" ></textarea></label>
+		<h3>Are you requesting:</h3>
+			<label><input type="checkbox" name="requestType0" value="paper">Paper copies of the documents </label><br>
+		<p>(For requests involving a large volume of papers or large amount of staff time, a fee may be charged. You will be informed of the fee in advance.)</p>
+			<label><input type="checkbox" name="requestType1" value="electronic">Electronic copies of the documents </label><br>
+		<p>(Many city records are not currently stored electronically, so producing electronic copies may delay the response to your request.)</p>
+			<label><input type="checkbox" name="requestType2" value="in-person">In-person examinations of the documents </label><br>
+		<p>(A city employee will contact you to set up an appointment time.)</p>
+		</form>
+	</article>
+	<footer>City of Elkins, WV &bull; 401 Davis Avenue, Elkins WV 26241 &bull; 304.636.1414<br> <a href="mailto:lmbarrick@cityofelkinswv.com" target="_blank">info@cityofelkinswv.com</a> &bull; <a href="http://cityofelkinswv.com/index.php/photo-credits">Photo Credits</a></footer>
+</body>
+</html>
+
